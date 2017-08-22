2cb92f70a928079ef048ebcac4e608665725d44a
Kurt Carpenter <kurtcarpenter@hotmail.com>
2017-08-21 21:40:09 -0700

    render post summary

--- public/layout.js
+++ public/layout.js
@@ -8,10 +8,20 @@ ReactDOM.render(
 )
 
 let BodyComponent = React.createClass({
-  render: () => (<div>blah blah</div>)
+  render: () => (<div>All Posts:<div id='posts'></div></div>)
 })
 
 ReactDOM.render(
   <BodyComponent />,
   document.getElementById('content')
 )
+
+// retrieve and render post summary
+axios.get('/posts/all')
+.then(resp => {
+  let postList = resp.data.map(item => <li>{item.title + ' - ' + item.author}</li>)
+  ReactDOM.render(<ul>{postList}</ul>, document.getElementById('posts'))
+})
+.catch(e => {
+  alert('Error getting posts: ' + e)
+})
